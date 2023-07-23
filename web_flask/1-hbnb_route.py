b0d709e612a813691f1ad8921667cddbe9ce29a1
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-07-23 23:43:50 +0100

    Conforming to PyCodeStyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,7 +5,6 @@ Creates flask application with two routings
 
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello():
@@ -21,4 +20,5 @@ def hbnb():
     return "HBNB"
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
