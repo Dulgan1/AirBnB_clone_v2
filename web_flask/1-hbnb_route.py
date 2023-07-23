2de72e8bdf4c7a6e5d4bad69da8930c26bf135f1
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-07-23 23:41:58 +0100

    Initial commit

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Creates flask application with two routings
+"""
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello():
+    """Returns string when route /
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Returns string when route /hbnb
+    """
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
